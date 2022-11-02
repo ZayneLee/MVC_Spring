048dbfb976e117ab397ad717ab36d9a97836127a
Zayne <star1582@naver.com>
2022-11-02 22:12:49 +0900

    controller

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+// @Slf4j
 @RestController
 public class LogTestController {
     private final Logger log = LoggerFactory.getLogger(getClass());
--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -5,19 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MappingController {
-
     private Logger log = LoggerFactory.getLogger(getClass());
-
-    /*
-     * 기본 요청
-     * 둘다 허용 /hello-basic, /hello-basic/
-     * HTTP 메서드 모두 허용 GET, HEAD, POST, PUT, PATCH, DELETE
-    */
 
     @RequestMapping("/hello-basic")
     public String helloBasic() {
@@ -25,38 +17,28 @@ public class MappingController {
         return "ok";
     }
 
-    /*
-     * method 특정 HTTP 메서드 요청만 허용
-     * GET, HEAD, POST, PUT, PATCH, DELETE
-    */
-    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
-    public String maapingGetV1(){
-        log.info("maapingGetV1");
-        return "ok";
-    }
-    
-    /*
-     * 편리한 축약 애노테이션 (코드보기)
-     * @GetMapping
-     * @PostMapping
-     * @PutMapping
-     * @DeleteMapping
-     * @PatchMapping
-    */
-    @GetMapping(value = "/mapping-get-v2")
+    @GetMapping("/mapping-get-v2")
     public String mappingGetV2() {
         log.info("mapping-get-v2");
         return "ok";
     }
 
-    /*
-     * PathVariable 사용
-     * 변수명이 같으면 생략 가능
-     * @PathVariable("userId") String userId -> @PathVariable userId
-     */
+    // @GetMapping("/mapping/{userId}")
+    // public String mappingPath(@PathVariable("userId") String data) {
+    // log.info("imappingPath userId = {}", data);
+    // return "ok";
+    // }
+
     @GetMapping("/mapping/{userId}")
-    public String mappingPath(@PathVariable("userId") String date) {
-        log.info("mappingPath userId={}",date);
+    public String mappingPath(@PathVariable String userId) {
+        log.info("imappingPath userId = {}", userId);
         return "ok";
     }
+
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
 }
